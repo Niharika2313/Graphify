db54f8928d23191095e80944becc79fddc838958
Niharika Nikhare <niharikanikhare2005@gmail.com>
2025-07-10 00:52:05 +0530

    Removed unnecessary files beforeb pushing to GitHub

--- app/graph_logic/__init__.py
+++
@@ -1,9 +0,0 @@
-# graph_logic/__init__.py
-from .line_plot import create_line_plot
-from .bar_plot import create_bar_plot
-#from .scatter_plot import create_scatter_plot
-#from .heatmap import create_heatmap
-
-# You can also define global settings here
-import seaborn as sns
-sns.set(style="whitegrid")
