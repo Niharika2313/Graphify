82f805903b7bf095c500c50a8a7a2bf8c8f7f44a
Niharika Nikhare <niharikanikhare2005@gmail.com>
2025-07-10 22:56:33 +0530

    Enhance upload form UI and graph gallery model popup layout

--- app/static/javascript/script.js
+++ app/static/javascript/script.js
@@ -1,9 +1,22 @@
 function showDropdown(graphName) {
-  const dropdownSection = document.getElementById('dropdownSection');
+  const modal = document.getElementById('dropdownModal');
   const selectedGraph = document.getElementById('selectedGraph');
   const graphTypeInput = document.getElementById('graphTypeInput');
 
-  dropdownSection.style.display = 'block';
+  modal.style.display = 'flex';
   selectedGraph.textContent = "Selected: " + graphName;
   graphTypeInput.value = graphName;
+}
+
+function closeDropdown() {
+  const modal = document.getElementById('dropdownModal');
+  modal.style.display = 'none';
+}
+
+// Optional: Close the modal if clicked outside the content
+window.onclick = function (event) {
+  const modal = document.getElementById('dropdownModal');
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
 }
