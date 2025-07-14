bb93d587a58143f5aa09c6f186ad60a95fd6699f
Niharika Nikhare <niharikanikhare2005@gmail.com>
2025-07-14 22:30:28 +0530

    Prepare for Render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from .routes import main
+import os
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'Nothing'
+    app.secret_key = os.environ.get("SECRET_KEY","dev-key")
 
     app.register_blueprint(main)
 
